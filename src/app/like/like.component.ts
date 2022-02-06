c3631650bad236fc0ad81bd10eaf761da6e5bf8e
Apondi Otieno <apondotii@gmail.com>
2022-02-07 01:11:14 +0300

    this will add style to header

--- src/app/like/like.component.ts
+++ src/app/like/like.component.ts
@@ -7,16 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LikeComponent implements OnInit {
 
-// numberOfLikes :number =0;
-//   constructor() { }
-// likeButtonClick(){
-//   this.numberOfLikes++;
-// }
 
-// numberOfDislikes :number =0;
-// dislikeButtonClick(){
-//   this.numberOfDislikes++;
-//}
   ngOnInit(): void {
   }
 
--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,7 +1,5 @@
 export class Quotes {
-    // id!: number;
-    // name!: string;
-    // description!: string;
+    
 
     showDescription: boolean;
     constructor(public id: number,public name: string,public description: string, public completeDate:Date, public likes: number,
