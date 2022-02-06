191134aad72e64d3c7c600285bfc6d877f1fc916
Apondi Otieno <apondotii@gmail.com>
2022-02-06 12:39:46 +0300

    this will add date

--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -4,7 +4,7 @@ export class Quotes {
     // description!: string;
 
     showDescription: boolean;
-    constructor(public id: number,public name: string,public description: string){
+    constructor(public id: number,public name: string,public description: string, public completeDate:Date){
       this.showDescription=false;
     }
 }
